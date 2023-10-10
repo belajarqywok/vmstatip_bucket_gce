6b68a2633d2459b204a439b25bd465d1113607d2
al-fariqy raihan ( qywok ) <59132829+belajarqywok@users.noreply.github.com>
2023-10-10 11:42:06 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,7 @@ output "network_IP" {
   value = google_compute_instance.vm_instance.instance_id
   description = "The internal ip address of the instance"
 }
+
 output "instance_link" {
   value = google_compute_instance.vm_instance.self_link
   description = "The URI of the created resource."
